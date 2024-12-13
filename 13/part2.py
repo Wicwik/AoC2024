8ec5b10b2adee2f005b18f1cbbf620c4c97e78f8
Robert Belanec <belanecrobert22@gmail.com>
2024-12-13 10:16:20 +0100

    day13

---
+++ 13/part2.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+machines = []
+
+a = [[],[]]
+b = []
+with open("input.txt", "r") as file:
+    for line in file:
+        if "Button" in line.strip():
+            x, y = line.split(":")[1].split(",")
+            a[0].append(int(x.replace("X+", "").strip()))
+            a[1].append(int(y.replace("Y+", "").strip()))
+        
+        if "Prize" in line.strip():
+            x, y = line.split(":")[1].split(",")
+            b.append(int(x.replace("X=", "").strip()) + 10000000000000)
+            b.append(int(y.replace("Y=", "").strip()) + 10000000000000)
+
+            machines.append((a, b))
+            a = [[],[]]
+            b = []
+    
+cost = 0
+
+for m in machines:
+    na, nb = np.round(np.linalg.solve(m[0], m[1]), 3)
+
+    if na.is_integer() and nb.is_integer():
+        cost += na*3 + nb*1
+
+print(int(cost))
