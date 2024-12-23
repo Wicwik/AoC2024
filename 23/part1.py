9dcdf2769488ab13453f00c855068531557f29cb
Robert Belanec <belanecrobert22@gmail.com>
2024-12-23 07:07:22 +0100

    day23

---
+++ 23/part1.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+
+computers = defaultdict(list)
+
+with open("input.txt", "r") as file:
+    for line in file:
+        a,b = line.strip().split("-")
+
+        computers[a].append(b)
+        computers[b].append(a)
+
+
+print(computers)
+
+tripples = []
+
+for comp, conn in computers.items():
+    for c in conn:
+        for cc in conn:
+            if c != cc:
+                if cc in computers[c]:
+                    if not ("t" == comp[0] or "t" == c[0] or "t" == cc[0]):
+                        continue
+
+                    tripple = set([comp, c, cc])
+                    if tripple not in tripples:
+                        tripples.append(tripple)
+
+print(len(tripples))
---
+++ 23/part2.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+
+computers = defaultdict(list)
+
+with open("input.txt", "r") as file:
+    for line in file:
+        a,b = line.strip().split("-")
+
+        computers[a].append(b)
+        computers[b].append(a)
+
+
+print(computers)
+
+comp_sets = []
+
+for comp, conn in computers.items():
+    s = set()
+    s.add(comp)
+    comp_sets.append(s)
+
+    for comp_set in comp_sets:
+        if all([c in computers[comp] for c in comp_set]):
+            comp_set.add(comp)
+
+
+print(",".join(sorted(list(max(comp_sets, key=len)))))
+
+
+
+
